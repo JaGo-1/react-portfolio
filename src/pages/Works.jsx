6f2609d079e6a53ef504b231e46722c47cc1737f
Jacqueline Gomez <137355862+JaGo-1@users.noreply.github.com>
2025-09-01 22:21:12 -0300

    refactor: redesign work section

--- src/pages/Works.jsx
+++ src/pages/Works.jsx
@@ -3,42 +3,69 @@ import { FaFigma, FaReact } from "react-icons/fa";
 import { RiTailwindCssFill } from "react-icons/ri";
 import HoverTooltip from "../components/HoverTooltip";
 
-const Works = () => {
+const TopInfo = () => {
   return (
-    <section className="w-full">
-      <div className="bg-zinc-200">
-        <div className="max-w-[1700px] m-auto h-[400px] py-16 px-8 lg:px-20 text-center md:text-start flex flex-col items-center justify-center gap-10">
-          <h4 className="inline-block relative text-4xl md:text-5xl lg:text-6xl font-srcpro font-medium before:content-[''] before:absolute before:bottom-2 before:-left-35 before:w-28 before:h-[1px] before:bg-zinc-900 lg:self-end">
-            Del concepto al código.
-          </h4>
-          <p className="mt-10 text-lg lg:text-3xl font-inter text-zinc-900 inline-block lg:self-start">
-            Conocé lo que estuve creando
+    <div className="bg-zinc-900 text-zinc-200 py-15 relative max-w-[1480px] m-auto">
+      <div className="max-w-[1700px] mx-auto grid grid-cols-1 md:grid-cols-3 gap-10 lg:gap-15 px-8 lg:px-20 text-center md:text-left relative">
+        <div>
+          <h6 className="font-srcpro text-xl xl:text-2xl tracking-widest mb-2">
+            OBJETIVO
+          </h6>
+          <p className="lg:text-lg xl:text-xl">
+            Transformar ideas en experiencias digitales simples y atractivas.
+          </p>
+        </div>
+
+        <div>
+          <h6 className="font-srcpro text-xl xl:text-2xl tracking-widest mb-2">
+            METODOLOGÍA
+          </h6>
+          <p className="lg:text-lg xl:text-xl">
+            Con organización, comunicación clara y atención al detalle en cada
+            etapa.
+          </p>
+        </div>
+
+        <div>
+          <h6 className="font-srcpro text-xl xl:text-2xl tracking-widest mb-2">
+            VALOR
+          </h6>
+          <p className="lg:text-lg xl:text-xl">
+            Soluciones funcionales, confiables y centradas en el usuario.
           </p>
         </div>
       </div>
 
-      {/* Project */}
-      <div className="bg-zinc-100 py-10 md:py-16">
-        <h5
-          className="relative inline-block text-2xl font-srcpro pl-36
-          before:content-[''] before:absolute before:bottom-2 before:left-0 before:w-28 before:h-[1px] before:bg-zinc-900"
-        >
-          UBIFACIL.
-        </h5>
+      <div className="hidden lg:block absolute border-2 border-zinc-700 top-4 -left-4 right-[14px] bottom-[-14px] pointer-events-none"></div>
+    </div>
+  );
+};
 
-        <div className="max-w-[1900px] mx-auto md:mx-0 grid md:grid-cols-2 gap-12 items-center mt-5">
+const Works = () => {
+  return (
+    <section className="w-full bg-zinc-100 lg:px-20">
+      <TopInfo />
+      {/* Project Section */}
+      <div className="bg-zinc-100 px-5 py-20 md:py-30">
+        <h5 className="text-4xl font-medium font-srcpro text-zinc-900 text-center md:text-left md:text-5xl lg:text-6xl xl:text-7xl  mb-10 md:mb-20">
+          Proyectos.
+        </h5>
+        <div className="mx-auto grid md:grid-cols-2 gap-12 items-start mt-5">
           {/* Image */}
-          <div className="shadow-lg overflow-hidden">
+          <div className="shadow-lg overflow-hidden h-full">
             <img
               src={ubifacil}
               alt="Ubifacil project picture"
-              className="w-full h-auto"
+              className="w-full h-full object-cover"
             />
           </div>
 
           {/* Description */}
-          <div className="text-left px-5 flex flex-col gap-10">
-            <p className="text-zinc-700 lg:text-3xl">
+          <div className="text-left px-5 py-5 flex flex-col gap-10">
+            <h5 className="text-xl md:text-2xl xl:text-3xl font-srcpro font-medium">
+              Ubifacil.
+            </h5>
+            <p className="text-zinc-700 md:text-lg xl:text-2xl max-w-2xl ">
               Plataforma web diseñada para facilitar a los usuarios el acceso a
               información sobre hoteles, restaurantes y eventos locales,
               permitiendo explorar, guardar favoritos y dejar reseñas de manera
@@ -54,8 +81,9 @@ const Works = () => {
               target="_blank"
               className="relative group w-fit font-srcpro"
             >
-              <p className=" text-zinc-800 lg:text-3xl">Ver Proyecto</p>
-              <span className="absolute left-0 -bottom-2 w-0 h-[2px] bg-zinc-800 transition-all duration-300 group-hover:w-full"></span>
+              <a className="bg-zinc-900 text-zinc-100 py-3 px-6 lg:text-lg xl:text-xl rounded-full transition-colors duration-200 hover:bg-zinc-800">
+                Ver Proyecto
+              </a>
             </a>
           </div>
         </div>
