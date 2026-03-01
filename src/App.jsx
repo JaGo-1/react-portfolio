6cce2d72029a87f92081fd41c5ad2625f3c7ab15
Jacqueline Gomez <137355862+JaGo-1@users.noreply.github.com>
2026-03-01 13:29:49 -0300

    feat: implement smooth scroll with Lenis and custom cursor

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,25 @@ import Home from "./pages/Home";
 import Works from "./pages/Works";
 import About from "./pages/About";
 import Footer from "./components/Footer";
+import { CursorProvider } from "./context/CursorContex";
+import CustomCursor from "./components/CustomCursor";
+import { ReactLenis } from "lenis/react";
 
 function App() {
   return (
-    <div className="overflow-x-hidden">
-      <FaviconSwitcher />
-      <Navbar />
-      <Home />
-      {/* <Works />
-      <About />
-      <Footer /> */}
-    </div>
+    <CursorProvider>
+      <CustomCursor />
+      <ReactLenis root options={{ duration: 1.2 }}>
+        <div className="overflow-x-hidden">
+          <FaviconSwitcher />
+          <Navbar />
+          <Home />
+          <Works />
+          <About />
+          <Footer />
+        </div>
+      </ReactLenis>
+    </CursorProvider>
   );
 }
 
